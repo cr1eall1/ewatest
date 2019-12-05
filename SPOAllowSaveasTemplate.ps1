d3645517cb46d4cb836b13a59073e45d2b7d9b26
cr1eall1 <erik@erikwallen.com>
2019-12-05 15:53:57 -0600

    Add files via upload

---
+++ SPOAllowSaveasTemplate.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module Microsoft.Online.SharePoint.Powershell
+$tenantName = "aegon"
+$SiteURL ="https://$($tenantName).sharepoint.com/sites/O365ExternalTeamTest"
+Connect-SPOService -Url "https://$($tenantName)-admin.sharepoint.com"
+Set-SPOsite $SiteURL -DenyAddAndCustomizePages 0
+
+
+Start-SPOUnifiedGroupMove -GroupAlias InternationalUnderwriting -DestinationDataLocation NAM
